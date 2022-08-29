0f4ffa5120301357024d1cfc19694ce2cbd239ab
Illia Avdeienko <Whiter4112@gmail.com>
2022-08-29 18:44:23 +0300

    Added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public float moveSpeed;
+    public Rigidbody rig;
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+
+        Vector3 vel = rig.velocity;
+        vel.y = 0;
+
+        if (vel.x != 0 || vel.z != 0)
+        {
+            transform.forward = vel;
+        }
+    }
 }
